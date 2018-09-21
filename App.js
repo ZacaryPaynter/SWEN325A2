ef878f83b6127800767fe6f24f8b85635eeca07a
escottkiri <escottkiri@ecs.vuw.ac.nz>
2018-09-21 14:23:50 +1200

    add native-base, create a form

--- App.js
+++ App.js
@@ -5,45 +5,46 @@
  * @format
  * @flow
  */
+import React, { Component } from 'react';
+import {
+  Container, Header, Left, Body, Input, Label, Text, Button,
+  Right, Title, Content, Form, Item
+} from 'native-base';
+export default class HeaderTitleExample extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+  }
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <Container>
+        <Header primary>
+          <Body>
+            <Title>Login</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Content>
+          <Form>
+            <Item floatingLabel>
+              <Label>Username</Label>
+              <Input
+                onChangeText={(text) => this.setState({ text })}
+                value={this.state.text} />
+            </Item>
+            <Item floatingLabel last>
+              <Label>Password</Label>
+              <Input />
+            </Item>
+          </Form>
+          <Button block>
+            <Text>Login</Text>
+          </Button>
+
+        </Content>
+      </Container>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
