886cd31e87d11a9b1fcdbe68fe20e53d13cfac8a
escottkiri <escottkiri@ecs.vuw.ac.nz>
2018-09-23 13:09:02 +1200

    add login -> basic budget list

---
+++ SwenApp/Screens/Budget/BudgetScreen.js
@@ -0,0 +1,76 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+import React, { Component } from 'react';
+import {
+    Container, Header, Left, Body, Input, Label, Text, Icon,
+    Right, Title, Content, Form, Item, Button, List, ListItem
+  } from 'native-base';
+import { View, ActivityIndicator } from 'react-native';
+
+  export class BudgetScreen extends Component {
+    static navigationOptions = {
+        title: 'Budget',
+      };
+    constructor(props) {
+      super(props);
+      this.state = { user: '', isLoading: true };
+    }
+
+    componentDidMount(){
+        return fetch('http://agile-cove-43620.herokuapp.com/api/budget')
+          .then((response) => response.json())
+          .then((responseJson) => {
+    
+            this.setState({
+              isLoading: false,
+              dataSource: responseJson,
+            }, function(){
+    
+            });
+    
+          })
+          .catch((error) =>{
+            console.error(error);
+          });
+      }
+
+      render(){
+
+        if(this.state.isLoading){
+          return(
+            <View style={{flex: 1, padding: 20}}>
+              <ActivityIndicator/>
+            </View>
+          )
+        }
+    
+        var items = [];
+        items = this.state.dataSource;
+    
+        return(
+          <View>
+            <List dataArray={items}
+              renderRow={(item) => 
+                <ListItem>
+                  <Text>{item.title}</Text>
+                  <Text>{item.amount}</Text>
+                </ListItem>
+              }>
+              </List>
+          </View>
+        );
+      }
+    }
+    
+
+
+
+
+
+ 
+  
---
+++ SwenApp/Screens/LoginScreen.js
@@ -0,0 +1,46 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+import React, { Component } from 'react';
+import {
+    Container, Header, Left, Body, Input, Label, Text,
+    Right, Title, Content, Form, Item, Button
+  } from 'native-base';
+
+  export class LoginScreen extends Component {
+    static navigationOptions = {
+        title: 'Login',
+      };
+    constructor(props) {
+      super(props);
+      this.state = { user: '' };
+    }
+  
+    render() {
+      return (
+        <Container>
+          <Content>
+            <Form>
+              <Item Label>
+                <Label>Username</Label>
+                <Input
+                  onChangeText={(user) => this.setState({ user })}
+                  value={this.state.user} />
+              </Item>
+            </Form>
+            <Button dark block onPress={() => this.props.navigation.navigate('Budget', {
+                user: this.state.user
+            })}>
+            <Text>Login</Text>
+          </Button>
+          </Content>
+        </Container>
+      );
+    }
+  }
+  
+  
